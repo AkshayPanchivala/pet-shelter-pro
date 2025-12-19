e7d30343555ccff86cf350e6c8abab9c6e2fd080
Akshay Panchivala <akshaypanchivala@gmail.com>
2025-12-19 22:00:46 +0530

    fixed bug

--- frontend/src/components/PetCard.tsx
+++ frontend/src/components/PetCard.tsx
@@ -11,12 +11,10 @@ export default function PetCard({ pet }: PetCardProps) {
     switch (status) {
       case 'Available':
         return 'bg-green-100 text-green-800';
-      case 'Pending':
-        return 'bg-yellow-100 text-yellow-800';
       case 'Adopted':
         return 'bg-gray-100 text-gray-800';
       default:
-        return 'bg-gray-100 text-gray-800';
+        return 'bg-green-100 text-green-800';
     }
   };
 
@@ -36,7 +34,7 @@ export default function PetCard({ pet }: PetCardProps) {
             pet.status
           )}`}
         >
-          {pet.status}
+          {pet.status ==='Adopted' ? pet.status :'Available' }
         </div>
       </div>
       <div className="p-4">
--- frontend/src/pages/PetDetail.tsx
+++ frontend/src/pages/PetDetail.tsx
@@ -99,12 +99,10 @@ console.log(userApplications, 'existingApplication');
     switch (status) {
       case 'Available':
         return 'bg-green-100 text-green-800';
-      case 'Pending':
-        return 'bg-yellow-100 text-yellow-800';
       case 'Adopted':
         return 'bg-gray-100 text-gray-800';
       default:
-        return 'bg-gray-100 text-gray-800';
+         return 'bg-green-100 text-green-800';
     }
   };
 
@@ -139,7 +137,7 @@ console.log(userApplications, 'existingApplication');
                   pet.status
                 )}`}
               >
-                {pet.status}
+                {pet.status ==='Adopted' ? pet.status :'Available' }
               </span>
             </div>
 
